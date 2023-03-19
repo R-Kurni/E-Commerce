c624b88fe0192197e16b423db401e088a381857a
Robby Kurniawan <thelazpiz@gmail.com>
2023-03-19 15:19:40 +0700

    Navbar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,16 +1,47 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
+	const handleLogout = () => {
+		localStorage.clear();
+	};
 	return (
 		<>
-			<Navbar bg="light" variant="light">
-				<Container>
-					<Navbar.Brand href="#home">Navbar</Navbar.Brand>
-					<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
-						<Nav.Link href="#features">Features</Nav.Link>
-						<Nav.Link href="#pricing">Pricing</Nav.Link>
-					</Nav>
+			<Navbar className="NavBar-BG" sticky="top">
+				<Container className="NavBar">
+					<div>
+						<Link to="/" className="nav-link">
+							<b className="nav-text">E-COMMERCE</b>
+						</Link>
+					</div>
+					{!localStorage.access_token ? (
+						<div>
+							<Nav>
+								<Link to="/login" className="nav-link">
+									<div className="nav-text">Login</div>
+								</Link>
+							</Nav>
+						</div>
+					) : (
+						<div>
+							<Nav
+								style={{
+									gap: "50px",
+									alignItems: "center",
+								}}
+							>
+								<Link to="/cart" className="nav-link">
+									<div className="nav-text">My Cart</div>
+								</Link>
+								<Link to="/store" className="nav-link">
+									<div className="nav-text">My Store</div>
+								</Link>
+								<Link to="/" className="nav-link" onClick={handleLogout}>
+									<div className="nav-text">Logout</div>
+								</Link>
+							</Nav>
+						</div>
+					)}
 				</Container>
 			</Navbar>
 		</>
